d34f06883b5823404b8e6f8a5f6d9fa0374bb333
Diana Mejías Hernández <85594834+dmejias08@users.noreply.github.com>
2023-04-16 10:35:22 -0600

    Set up ready to start developing

---
+++ GymTEC-Backend/GymTEC-Backend/Startup.cs
@@ -0,0 +1,40 @@
+﻿
+
+namespace GymTEC_Backend
+{
+    public class Startup
+    {
+        public IConfiguration configRoot
+        {
+            get;
+        }
+        public Startup(IConfiguration configuration)
+        {
+            configRoot = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddRazorPages();
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
