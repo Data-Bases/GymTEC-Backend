63084a1a7de9dc3022dbb2e7bb4d54dee1ce3b20
Diana Mejías Hernández <85594834+dmejias08@users.noreply.github.com>
2023-04-20 21:23:36 -0600

    añadir scripts de populacion y creacions. Ademas de añadio un script para queries temporales

--- DB/GymTEC-creation.sql
+++ DB/GymTEC-creation.sql
@@ -1,137 +1,117 @@
-DROP TABLE IF EXISTS Cliente;
-DROP TABLE IF EXISTS Sucursal;
-DROP TABLE IF EXISTS Empleado;
-DROP TABLE IF EXISTS ClienteClase;
-DROP TABLE IF EXISTS TipoEquipo;
-DROP TABLE IF EXISTS TratamientoSpa;
-DROP TABLE IF EXISTS ServiciosClases;
-DROP TABLE IF EXISTS TipoPlanilla;
-DROP TABLE IF EXISTS Puesto;
-DROP TABLE IF EXISTS Horario;
-DROP TABLE IF EXISTS Producto;
-DROP TABLE IF EXISTS Clase;
-DROP TABLE IF EXISTS Maquina;
-DROP TABLE IF EXISTS NumerosTelefono;
-DROP TABLE IF EXISTS ServiciosSucursal;
-DROP TABLE IF EXISTS TratamientoSucursal;
-DROP TABLE IF EXISTS HorariosSucursal;
-DROP TABLE IF EXISTS ProductoSucursal;
---ALTER TABLE EMPLOYEE DROP CONSTRAINT EMPLOYEE_DEPARTMENT_FK;
-
 CREATE TABLE Maquina
 (
-	NumeroSerie			INT NOT NULL,
-	Marca				VARCHAR(100) NOT NULL,
-	Costo				INT NOT NULL,
-	NombreSucursal		VARCHAR(100) NOT NULL,
-	IdEquipo			INT,
+	NumeroSerie INT NOT NULL,
+	Marca VARCHAR(100) NOT NULL,
+	Costo INT NOT NULL,
+	NombreSucursal VARCHAR(100) NOT NULL,
+	IdEquipo INT,
 
 	PRIMARY KEY (NumeroSerie)
 );
 
 CREATE TABLE ServiciosSucursal
 (
-	Id					INT NOT NULL,
-	IdServicioClase		INT NOT NULL,
-	NombreSucursal		VARCHAR(100) NOT NULL,
+	Id INT NOT NULL IDENTITY(1,1),
+	IdServicioClase INT NOT NULL,
+	NombreSucursal VARCHAR(100) NOT NULL,
 
 	PRIMARY KEY (Id)
 );
-
-
 
 CREATE TABLE TratamientoSucursal
 (
-	Id					INT NOT NULL,
-	IdTratamientoSpa    INT NOT NULL,
-	NombreSucursal	    VARCHAR(100) NOT NULL,
+	Id INT NOT NULL IDENTITY(1,1),
+	IdTratamientoSpa INT NOT NULL,
+	NombreSucursal VARCHAR(100) NOT NULL,
 
 	PRIMARY KEY (Id)
 );
 
-
 CREATE TABLE HorariosSucursal
 (
-	Id					INT NOT NULL,
-	IdHorario 			INT NOT NULL,
-	NombreSucursal		VARCHAR(100) NOT NULL,
+	Id INT NOT NULL IDENTITY(1,1),
+	IdHorario INT NOT NULL,
+	NombreSucursal VARCHAR(100) NOT NULL,
 
 	PRIMARY KEY (Id)
 );
 
 CREATE TABLE ProductoSucursal
 (
-	Id					    INT NOT NULL,
-    CodigoBarrasProducto    INT NOT NULL,
-    NombreSucursal          VARCHAR(100) NOT NULL,
+	Id INT NOT NULL IDENTITY(1,1),
+	CodigoBarrasProducto INT NOT NULL,
+	NombreSucursal VARCHAR(100) NOT NULL,
 
 	PRIMARY KEY (Id)
 );
 
 CREATE TABLE NumerosTelefono
 (
-	Id					INT NOT NULL,
-	NumeroTelefono		INT NOT NULL,
-	NombreSucursal		VARCHAR(100),
+	Id INT NOT NULL IDENTITY(1,1),
+	NumeroTelefono INT NOT NULL,
+	NombreSucursal VARCHAR(100),
 
 	PRIMARY KEY (Id)
 );
 
 CREATE TABLE Clase
 (
-	Id					INT NOT NULL,
-	NombreClase			VARCHAR(100) NOT NULL,
-	HoraInicio			TIME(0) NOT NULL, --'15:30'
-	HoraFinalizacion	TIME(0) NOT NULL,
-	Fecha				DATE NOT NULL,					
-	Capacidad			INT NOT NULL,				
-	EsGrupal			BIT NOT NULL, -- 1 true or 0 false
-	CedulaEmpleado		INT,
+	Id INT NOT NULL IDENTITY(1,1),
+	NombreClase VARCHAR(100) NOT NULL,
+	HoraInicio TIME(0) NOT NULL,
+	HoraFinalizacion TIME(0) NOT NULL,
+	Fecha DATE NOT NULL,
+	Capacidad INT NOT NULL,
+	EsGrupal BIT NOT NULL,
+	CedulaEmpleado INT,
 
 	PRIMARY KEY (Id)
 );
- 
+
 
 CREATE TABLE Producto
 (
-	CodigoBarras        INT NOT NULL,
-    Nombre				VARCHAR(100) NOT NULL,
-	Costo				INT NOT NULL,
-	Descripcion			VARCHAR(100),
+	CodigoBarras INT NOT NULL,
+	Nombre VARCHAR(100) NOT NULL,
+	Costo INT NOT NULL,
+	Descripcion VARCHAR(100),
 
 	PRIMARY KEY (CodigoBarras)
 );
 
 CREATE TABLE Horario
 (
-	Id					INT NOT NULL,
-	Dia				    DATE NOT NULL,					
-	HoraApertura		TIME(0) NOT NULL, --'15:30'
-	HoraCierra	        TIME(0) NOT NULL,
+	Id INT NOT NULL IDENTITY(1,1),
+	Dia DATE NOT NULL,
+	HoraApertura TIME(0) NOT NULL, --'15:30'
+	HoraCierre TIME(0) NOT NULL,
 
 	PRIMARY KEY (Id)
 );
 
 CREATE TABLE Puesto
 (
-	Id					INT NOT NULL,
-	Descripcion			VARCHAR(100) NOT NULL UNIQUE,
+	Id INT NOT NULL IDENTITY(1,1),
+	Nombre VARCHAR(100) NOT NULL,
+	Descripcion VARCHAR(100) NOT NULL UNIQUE,
 
 	PRIMARY KEY (Id)
 );
 
 CREATE TABLE TipoPlanilla
 (
-	Id					INT NOT NULL,
-	Descripcion			VARCHAR(100) NOT NULL UNIQUE,
+	Id INT NOT NULL IDENTITY(1,1),
+	Nombre VARCHAR(100) NOT NULL,
+	Descripcion VARCHAR(100) NOT NULL UNIQUE,
 
 	PRIMARY KEY (Id)
 );
 
 CREATE TABLE ServiciosClases
 (
-	Id					INT NOT NULL,
-	Descripcion			VARCHAR(100) NOT NULL UNIQUE,
+	Id INT NOT NULL IDENTITY(1,1),
+	Nombre VARCHAR(100) NOT NULL,
+	Descripcion VARCHAR(100) NOT NULL UNIQUE,
 
 	PRIMARY KEY (Id)
 );
@@ -139,25 +119,27 @@ CREATE TABLE ServiciosClases
 
 CREATE TABLE TratamientoSpa
 (
-	Id					INT NOT NULL,
-	Nombre			    VARCHAR(100) NOT NULL UNIQUE,
+	Id INT NOT NULL IDENTITY(1,1),
+	Nombre VARCHAR(100) NOT NULL UNIQUE,
+	Descripcion VARCHAR(100) NOT NULL UNIQUE,
 
 	PRIMARY KEY (Id)
 );
 
 CREATE TABLE TipoEquipo
 (
-	Id					INT NOT NULL,
-	Descripcion			VARCHAR(100) NOT NULL UNIQUE,
+	Id INT NOT NULL IDENTITY(1,1),
+	Nombre VARCHAR(100) NOT NULL,
+	Descripcion VARCHAR(100) NOT NULL UNIQUE,
 
 	PRIMARY KEY (Id)
 );
 
 CREATE TABLE ClienteClase
 (
-	Id					INT NOT NULL,
-    IdClase             INT NOT NULL,
-    CedulaCliente       INT NOT NULL,
+	Id INT NOT NULL IDENTITY(1,1),
+	IdClase INT NOT NULL,
+	CedulaCliente INT NOT NULL,
 
 	PRIMARY KEY (Id)
 );
@@ -165,36 +147,36 @@ CREATE TABLE ClienteClase
 
 CREATE TABLE Empleado
 (
-	Cedula              INT NOT NULL,
-    Nombre				VARCHAR(100) NOT NULL,
-	Apellido1			VARCHAR(100) NOT NULL,
-	Apellido2           VARCHAR(100),
-	Provincia			VARCHAR(100) NOT NULL,
-	Canton				VARCHAR(100) NOT NULL,
-	Distrito			VARCHAR(100) NOT NULL,
-	Salario				INT NOT NULL,
-	Email				VARCHAR(100) NOT NULL,
-	Contrasena			VARCHAR(100) NOT NULL,
-	HorasLaboradas		INT,
-	NombreSucursal		VARCHAR(100),
-	IdTipoPlanilla		INT,
-	IdPuesto			INT,
+	Cedula INT NOT NULL,
+	Nombre VARCHAR(100) NOT NULL,
+	Apellido1 VARCHAR(100) NOT NULL,
+	Apellido2 VARCHAR(100),
+	Provincia VARCHAR(100) NOT NULL,
+	Canton VARCHAR(100) NOT NULL,
+	Distrito VARCHAR(100) NOT NULL,
+	Salario INT NOT NULL,
+	Email VARCHAR(100) NOT NULL,
+	Contrasena VARCHAR(100) NOT NULL,
+	HorasLaboradas INT,
+	NombreSucursal VARCHAR(100),
+	IdTipoPlanilla INT,
+	IdPuesto INT,
 
 	PRIMARY KEY (Cedula)
 );
 
 CREATE TABLE Sucursal
 (
-	Nombre              VARCHAR(100) NOT NULL,
-	Provincia			VARCHAR(100) NOT NULL,
-	Canton				VARCHAR(100) NOT NULL,
-	Distrito			VARCHAR(100) NOT NULL,
-    Senas			    VARCHAR(100) NOT NULL,
-	CapacidadMaxima     INT NOT NULL,
-	FechaApertura		DATE NOT NULL,
-	TiendaAbierta	    BIT NOT NULL, -- 1 true, 0 false
-	SpaAbierto          BIT NOT NULL, -- 1 true, 0 false
-	IdEmpleadoAdmin		INT NOT NULL,
+	Nombre VARCHAR(100) NOT NULL,
+	Provincia VARCHAR(100) NOT NULL,
+	Canton VARCHAR(100) NOT NULL,
+	Distrito VARCHAR(100) NOT NULL,
+	Senas VARCHAR(100) NOT NULL,
+	CapacidadMaxima INT NOT NULL,
+	FechaApertura DATE NOT NULL,
+	TiendaAbierta BIT NOT NULL,
+	SpaAbierto BIT NOT NULL,
+	IdEmpleadoAdmin INT NOT NULL,
 
 	PRIMARY KEY (Nombre)
 );
@@ -203,18 +185,18 @@ CREATE TABLE Sucursal
 
 CREATE TABLE Cliente
 (
-	Cedula              INT NOT NULL,
-    Nombre				VARCHAR(100) NOT NULL,
-	Apellido1			VARCHAR(100) NOT NULL,
-	Apellido2           VARCHAR(100),
-	Provincia			VARCHAR(100) NOT NULL,
-	Canton				VARCHAR(100) NOT NULL,
-	Distrito			VARCHAR(100) NOT NULL,
-	Email				VARCHAR(100) NOT NULL,
-	Contrasena		VARCHAR(100) NOT NULL,
-	FechaNacimiento		DATE NOT NULL,
-	Peso	            FLOAT(2),
-	IMC	                FLOAT(2),
+	Cedula INT NOT NULL,
+	Nombre VARCHAR(100) NOT NULL,
+	Apellido1 VARCHAR(100) NOT NULL,
+	Apellido2 VARCHAR(100),
+	Provincia VARCHAR(100) NOT NULL,
+	Canton VARCHAR(100) NOT NULL,
+	Distrito VARCHAR(100) NOT NULL,
+	Email VARCHAR(100) NOT NULL,
+	Contrasena VARCHAR(100) NOT NULL,
+	FechaNacimiento DATE NOT NULL,
+	Peso FLOAT(2),
+	IMC FLOAT(2),
 
 	PRIMARY KEY (Cedula)
 );
@@ -248,7 +230,7 @@ ALTER TABLE NumerosTelefono
 ADD CONSTRAINT NumeroTelefonoSucursal FOREIGN KEY (NombreSucursal)
 REFERENCES Sucursal(Nombre);
 
--- ServiciosSucursal - ServciosClases
+-- ServiciosSucursal - ServiciosClases
 ALTER TABLE ServiciosSucursal
 ADD CONSTRAINT TipoClaseImpartida FOREIGN KEY (IdServicioClase)
 REFERENCES ServiciosClases(Id);
--- DB/GymTEC-population.sql
+++ DB/GymTEC-population.sql
@@ -1,7 +1,48 @@
-INSERT INTO Cliente(Cedula, Nombre, Apellido1, Apellido2, Provincia, Canton, Distrito, Email, Contrasena, FechaNacimiento)
-VALUES	(305320066,'Valesska', 'Blanco', 'Montoya', 'Cartago', 'Central', 'Aguacaliente', 'valesskablanco@estudiantec.cr', 'pepe', '2001-05-22');
+-- Insert SuperAdmin
+INSERT INTO Empleado(Cedula, Nombre, Apellido1, Apellido2, Provincia, Canton, Distrito, Salario, Email, Contrasena)
+            VALUES  (305320066,'Valesska', 'Blanco', 'Montoya', 'Cartago', 'Central', 'Aguacaliente', 2500000, 'valesskablanco@estudiantec.cr', 'pepe');
 
-INSERT INTO Cliente(Cedula, Nombre, Apellido1, Apellido2, Provincia, Canton, Distrito, Email, Contrasena, FechaNacimiento)
-VALUES	(208200172,'Diana', 'Mejias', 'Hernandez', 'Cartago', 'Central', 'Oriental', 'dmejias@estudiantec.cr', 'Chloe', '2001-08-08');
 
-SELECT * FROM Cliente;
+-- Insert Branches
+
+INSERT INTO Sucursal(Nombre, Provincia, Canton, Distrito, Senas, CapacidadMaxima, FechaApertura, SpaAbierto, TiendaAbierta, IdEmpleadoAdmin)
+            VALUES  ('Gym ASETEC', 'Cartago', 'Central', 'Oriental', '50m oeste y 200m norte de la entrada del ITCR', 100, '01-01-2000', 0,0, 305320066)
+
+
+-- Insert Tratamiento Spa
+INSERT INTO TratamientoSpa(Nombre, Descripcion)
+            VALUES  ('Masaje Relajante', 'Masaje relajante utilizando vapor y aceites esenciales'),
+                    ('Masaje descarga muscular', 'Masaje perfecto para la recuperacion de los musculos'),
+                    ('Sauna', 'Ideal sesion para desestresarse'),
+                    ('baños a vapor', 'Son utiles para liberar toxinas y limpiar poros')
+
+-- Insert Puestos
+INSERT INTO Puesto(Nombre, Descripcion)
+            VALUES  ('Administrador', 'Encargado de una sucursal'),
+                    ('Instructor', 'Encargado de impartir una clase'),
+                    ('Dependiente Spa', 'Encargado de los clientes en el spa'),
+                    ('Dependiente tienda', 'Encargado de vender y atender clientes en la tienda')
+
+-- Insert Tipos de Planillas
+INSERT INTO TipoPlanilla(Nombre, Descripcion)
+            VALUES  ('Mensual', 'Mes a mes se deposita el salario correspondiente'),
+                    ('Pago por horas', 'Se calcula multiplicando las horas laboradas por el salario correspondiente'),
+                    ('Pago por clase', 'Se calcula segun las clases impartidas multiplicado por el salario correspondiente')
+
+-- Insert Empleados
+
+-- Insert Servicios 
+INSERT INTO ServiciosClases(Nombre, Descripcion)
+            VALUES  ('Indoor Cycling', 'Clase de bicicleta estacionaria'),
+                    ('Pilates', 'Clase de pilates relajante'),
+                    ('Yoga', 'Clase de yoga para la flexibilidad'),
+                    ('Zumba', 'Clase de Zumba con musica latina'),
+                    ('Natacion', 'Clase de natacion ideal para principiantes')
+
+-- Insert Equipo 
+INSERT INTO TipoEquipo(Nombre, Descripcion)
+            VALUES  ('Cintas de correr', 'Maquina para correr de manera estacionaria'),
+                    ('Bicicleta', 'Maquina para practicar el ciclismo estacionario'),
+                    ('Multigimnasios', 'Maquina para realizar gran cantidad de ejercicios'),
+                    ('Remos', 'Maquina para entrenar el tren superior'),
+                    ('Pesas', 'Necesario para cualquier entrenamiento')
